d042ee99a4a3e86625c6621aa6b6552ac687fc70
Ottokar Elgeti <ottnaton@gmail.com>
2024-06-16 21:30:22 +0200

    Added Quit button

--- screens/start/start_screen.gd
+++ screens/start/start_screen.gd
@@ -4,11 +4,18 @@ extends Node2D
 
 @onready var base: PanelContainer = $PanelContainer
 @onready var play_button: Button = $PanelContainer/PlayButton
+@onready var quit_botton: Button = $PanelContainer/QuitButton
+@onready var tab_container: TabContainer = $PanelContainer/TabContainer
 @onready var size: Vector2 = DisplayServer.window_get_size()
 
 func _ready() -> void:
 	base.size = size
+	tab_container.custom_minimum_size = size * 9 / 10
 	play_button.custom_minimum_size = size / 10
+	quit_botton.custom_minimum_size = size / 12
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_packed(sandbox)
+
+func _on_quit_button_pressed():
+	get_tree().quit()
